47c863055ca4bc1e4c1bc72b7713d999367e3d1f
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-03-21 21:09:57 +0530

    added register user controller and fixed minor bugs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,11 +1,61 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js" ; 
+import {User} from "../models/user.model.js" ; 
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiReponse } from "../utils/ApiResponse.js";
 
+ 
 
 
 const registerUser = asyncHandler( async(req , res) =>{
-    res.status("200").json({
-        message : "ok"
+    const {username , fullname  , email , password}  = req.body 
+    console.log(email)
+
+    if (
+        [ email , username , password].some((fields) => fields?.trim()==="")
+    ) {
+        throw new ApiError(400 , "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or : [{username} , {email}]
     })
+    console.log(existedUser) ;
+    if(existedUser){
+        throw new ApiError (409  , "User with email or Username already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path ;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path ; 
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400 , "Avatar file is required") ; 
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+        throw new ApiError(400 , "Avatar file is required") ; 
+    }
+
+    await User.create({
+        fullname , 
+        email , 
+        avatar  : avatar.url , 
+        coverImage : coverImage.url , 
+        password , 
+        username : username.toLowercase()
+    })
+
+    const createdUser = await User.findById(User._id).select("-password -refreshToken")
+    if (!createdUser) {
+        throw new ApiError(500 , "Something went Wrong While registering user")
+    }
+
+    return res.status(201).json(
+        new ApiReponse(200 , createdUser , "User created Sucessfully ")
+    )
 } )
 
 
