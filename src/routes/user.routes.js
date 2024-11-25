0730d1109a47d2157c8eccc2650a52dde94a82c6
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-11-25 21:50:04 +0530

    add remaining routes for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controllers.js";
+import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,7 +25,10 @@ router.route("/login").post(loginUser)
 // secured routes
 
 router.route("/logout").post(verifyJWT , logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(verifyJWT ,  refreshAccessToken)
+router.route("/update-profile").patch(verifyJWT , updateAccountDetails)
+router.route("/change-password").patch(verifyJWT , changeCurrentPassword)
+router.route("/current-user").get(verifyJWT , getCurrentUser)
 
 
 export default router  ;
