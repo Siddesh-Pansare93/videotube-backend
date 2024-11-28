2972a124f7cab9d74289e7e34f1fc9bfc0657e41
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-11-28 23:19:08 +0530

    add Like model

---
+++ src/models/like.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const likeSchema = mongoose.Schema({
+    comment :  {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    } , 
+    Video :  {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'Video'
+    },
+    tweet :  { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'Tweet'
+    },
+    likedBy :  {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'User'
+    }
+} ,{timeStamps : true })
+
+export const Like  = mongoose.model("Like" , likeSchema)
