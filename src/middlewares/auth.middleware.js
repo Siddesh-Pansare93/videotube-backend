5c941ebb2691d5df7e9235a74e949adcf5593d78
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-12-01 00:07:56 +0530

    minor updates(removed console logs)

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -8,7 +8,7 @@ import { User } from "../models/user.model.js"
 export const verifyJWT = asyncHandler(async(req ,res , next )=>{
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-        console.log("token is  ",token)
+        // console.log("token is  ",token)
         if (!token) {
             throw new ApiError(400 ,  "Unauthorized Request")
         }
