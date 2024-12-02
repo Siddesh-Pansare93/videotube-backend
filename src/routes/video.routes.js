fa1b75d998f86ff5eafcfd537aeb8147dd4ffa48
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-12-03 00:19:23 +0530

    add routes for controller methods

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 import {upload} from "../middlewares/multer.middleware.js"
-import {publishAVideo} from "../controllers/video.controller.js"
+import {getVideoById, publishAVideo, togglePublishStatus, updateVideo} from "../controllers/video.controller.js"
 
 const router  = Router()
 
@@ -12,6 +12,14 @@ router.route("/create").post(upload.fields([
     {name : "thumbnail" ,  maxCount : 1 }
 
 ]), publishAVideo)
+
+router
+    .route("/:videoId")
+    .get(getVideoById)
+    .patch(upload.single("thumbnail") , updateVideo)
+
+
+router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
 
 
 export default router
