e78e8a50a7e535acc1d4b6ba396db7db99519464
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-12-02 17:08:56 +0530

    add routes for Video controller

---
+++ src/routes/video.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+import {upload} from "../middlewares/multer.middleware.js"
+import {publishAVideo} from "../controllers/video.controller.js"
+
+const router  = Router()
+
+router.use(verifyJWT)
+
+router.route("/create").post(upload.fields([
+    {name : "video" , maxCount : 1},
+    {name : "thumbnail" ,  maxCount : 1 }
+
+]), publishAVideo)
+
+
+export default router
